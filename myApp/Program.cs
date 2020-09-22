91dbb5c60cc5260904924c54113bff935c5da798
Alberto Salazar <alberto.salazar@ac-techsolutions.com>
2020-09-21 21:41:24 -0500

    Rest Api inicial
    Rest Api inicial

---
+++ myApp/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace myApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World with C#!");
+            Console.WriteLine("From Java to .Net world");
+        }
+    }
+}
